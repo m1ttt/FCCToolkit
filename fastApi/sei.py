c256a13f2972624913ae2a013082fde593ded149
Mike <mitnez2004@gmail.com>
2024-04-25 05:48:17 +0000

    .

--- fastApi/sei.py
+++ fastApi/sei.py
@@ -1,6 +1,23 @@
 import math
 
 def sucesiones(inferior, superior, formula):
+    """
+    Esta función calcula una serie de valores basada en una fórmula dada, dentro de un rango definido por los límites inferior y superior.
+
+    Parámetros:
+    inferior (int): El límite inferior del rango.
+    superior (int): El límite superior del rango.
+    formula (str): Una cadena que representa la fórmula a evaluar. Esta fórmula debe ser una expresión válida de Python que puede incluir la variable 'k'.
+
+    Devuelve:
+    k (int): El último valor de k utilizado en la fórmula.
+    suma (float): La suma de todos los valores calculados.
+    multiplicacion (float): El producto de todos los valores calculados.
+    serie (list): Una lista de todos los valores calculados.
+
+    Nota:
+    Si el producto de los valores calculados es infinito, la función devolverá math.inf para la multiplicación.
+    """
     limite = superior - inferior
     sumatoria = 0
     multiplicacion = 1
